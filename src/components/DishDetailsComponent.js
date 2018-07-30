ce65da33fd4435972ffee9c02099b97a52c43051
Ashley Grealish <ashley@grealish.co.uk>
2018-07-30 19:56:13 +0100

    Adding Main component

--- src/components/DishDetailsComponent.js
+++ src/components/DishDetailsComponent.js
@@ -27,7 +27,7 @@ class DishDetails extends Component {
 				return (
 					<li key={comment.id}>
 						<p>{comment.comment}</p>
-						<p>-- {comment.author} {comment.date}</p>
+						<p>-- {comment.author}, {new Intl.DateTimeFormat('en-UK', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date))) }</p>
 					</li>
 				)
 			});
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishDetails from './DishDetailsComponent'
 
 class Menu extends Component {
 
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			selectedDish: null
-		};
-	}
-
-	onDishSelect(dish) {
-		this.setState({selectedDish: dish});
-	}
- 
-	renderDish() {
-
 	}
 
 	render() {
@@ -25,7 +12,7 @@ class Menu extends Component {
 		const menu = this.props.dishes.map((dish) => {
 			return (
 				<div key={dish.id} className="col-12 col-md-5 m-1">
-					<Card onClick={() => this.onDishSelect(dish)}>
+					<Card onClick={() => this.props.onClick(dish.id)}>
 						<CardImg width="100%" src={dish.image} alt={dish.name} />
 						<CardImgOverlay body className="ml-5">
 							<CardTitle heading>{dish.name}</CardTitle>
@@ -40,7 +27,6 @@ class Menu extends Component {
 				<div className="row">
 					{menu}
 				</div>
-				<DishDetails dish={this.state.selectedDish} />
 			</div>
 		);
 	}
